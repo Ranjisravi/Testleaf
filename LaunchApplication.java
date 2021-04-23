196972b65e77c5d4b5a3153bf4b116f1696bd677
Ranjith Ravichandhran <82880737+Ranjisravi@users.noreply.github.com>
2021-04-23 10:51:06 +0530

    Selenium files of practised

--- LaunchApplication.java
+++ LaunchApplication.java
@@ -1,33 +1,15 @@
 package week2.day1;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchApplication 
 {
+	public static void main(String[] args)
+	{
+		//Open Chromebrowser
+		ChromeDriver drive = new ChromeDriver();
+	
+		//Load the application URL
 
-	public static void main(String[] args) throws InterruptedException 
-    {
-    	//Setup the Chrome driver
-		//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
-		
-		//Step1: To setup driver
-		WebDriverManager.chromedriver().setup();
-		
-		//Step2: Open the Chrome browser
-		ChromeDriver driver = new ChromeDriver();
-		
-		//Step3: Load the application URL		
-		 driver.get("http://leaftaps.com/opentaps/control/main");
-		 
-		 //Step4: To maximise the window
-		 driver.manage().window().maximize();
-		 
-		 //Java wait
-		 Thread.sleep(2000);
-		 
-		 //Step5: To close the window
-		 driver.close();
-		
-    }
+	}
 }
